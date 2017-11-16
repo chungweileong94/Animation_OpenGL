9d8ce252aa4f188849c0b543d27cfc97317f81b4
Chung Wei Leong <leongchungwei@hotmail.com>
2017-11-16 19:51:10 +0800

    Added hex to float helper function

---
+++ Animation_OpenGL/Helper.cpp
@@ -0,0 +1,6 @@
+#include "Helper.h"
+
+float Helper::hexToFloat(int hex)
+{
+	return hex / 255.0f;
+}
---
+++ Animation_OpenGL/Helper.h
@@ -0,0 +1,9 @@
+#ifndef HELPER_H
+#define HELPER_H
+
+namespace Helper
+{
+	float hexToFloat(int hex);
+}
+
+#endif // !HELPER_H
